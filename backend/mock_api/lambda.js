13b988090e5efbd213bb3cfa746ab4f8d898b18e
ABHISHEK\abhishek <abhishektripathi@doshaheen.com>
2025-05-10 02:14:03 +0530

    Updated code with docker run

---
+++ backend/mock_api/lambda.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server');
+const serverlessExpress = require('@vendia/serverless-express');
+
+// Set up json-server
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // Make sure db.json exists in this folder
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+// Export the Lambda handler
+exports.handler = serverlessExpress({ app: server });
--- mobile/lib/api_constants.dart
+++ mobile/lib/api_constants.dart
@@ -1,5 +1,5 @@
 /// Global API constants for backend endpoints
 class ApiConstants {
   // Change this value for different environments (localhost, LAN IP, etc.)
-  static const String baseUrl = 'http://localhost:8000';
+  static const String baseUrl = 'https://services.tatkalpro.in';
 }
